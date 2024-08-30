5cd6df3f7873727c18c4fa58a9efb9ca63e44187
Garrett Thompson <garrettrossthompson@outlook.com>
2024-08-30 14:22:29 -0400

    Added some more enemy config stuff.

--- scripts/units/enemy_unit.gd
+++ scripts/units/enemy_unit.gd
@@ -6,3 +6,6 @@ var description: EnemyLookup.Enemy;
 func set_description(desc: EnemyLookup.Enemy):
 	description = desc;
 	animated_sprite.sprite_frames = description.appearance._sprite;
+
+func path_to_player():
+	pass
